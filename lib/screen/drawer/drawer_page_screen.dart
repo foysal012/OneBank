8cc6397d732238220a8693ef84ec3cb833a3f40a
Md. Foysal Joarder <foysaljoarder012@gmail.com>
2023-10-13 09:03:42 +0600

    drawer page 90% complete and home page designing and continue...

--- lib/screen/drawer/drawer_page_screen.dart
+++ lib/screen/drawer/drawer_page_screen.dart
@@ -8,22 +8,31 @@ class DrawerPageScreen extends StatefulWidget {
 }
 
 class _DrawerPageScreenState extends State<DrawerPageScreen> {
+
+  bool isOn = false;
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: Column(
         children: [
+
           Expanded(
               flex: 3,
               child: Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(
+                  left: 20,
+                ),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Container(
-                      height: 80,
-                      width: 80,
+                      height: 70,
+                      width: 70,
                       decoration: BoxDecoration(
-                        color: Colors.yellow,
+                        //color: Colors.yellow,
+                        image: DecorationImage(image: AssetImage("images/w3.jpg"),fit: BoxFit.cover),
                         borderRadius: BorderRadius.circular(20),
                         border: Border.all(
                           color: Colors.red,
@@ -32,20 +41,249 @@ class _DrawerPageScreenState extends State<DrawerPageScreen> {
                       ),
                     ),
 
-                    Text("Foysal Joarder"),
+                    Text("Foysal Joarder",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black87,
+                    ),
+                    ),
 
-                    Text("OBL58426652"),
-
+                    Text("OBL58426652",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.redAccent,
+                      ),
+                    ),
                   ],
                 ),
               )
           ),
+
           Expanded(
               flex: 4,
-              child: Placeholder()),
+              child: Container(
+                padding: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                ),
+                child: Column(
+                  children: [
+
+                    DrawerItemWidget(
+                      onTap: () {
+
+                      },
+                      title: "Banking",
+                    ),
+
+                    SizedBox(
+                      height: 5,
+                    ),
+
+                    DrawerItemWidget(
+                      onTap: () {
+
+                      },
+                      title: "Fund Transfer",
+                    ),
+
+                    SizedBox(
+                      height: 5,
+                    ),
+
+                    DrawerItemWidget(
+                      onTap: () {
+
+                      },
+                      title: "Bill Pay",
+                    ),
+
+                    SizedBox(
+                      height: 5,
+                    ),
+
+                    DrawerItemWidget(
+                      onTap: () {
+
+                      },
+                      title: "Airtime",
+                    ),
+
+                    SizedBox(
+                      height: 5,
+                    ),
+
+                    DrawerItemWidget(
+                      onTap: () {
+
+                      },
+                      title: "EMI Calculator",
+                    ),
+
+                    SizedBox(
+                      height: 5,
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        DrawerItemWidget(
+                          onTap: () {
+
+                          },
+                          title: "Notification",
+                        ),
+
+                        Switch(
+                            value: isOn,
+                            onChanged: (value){
+                              setState(() {
+                                isOn = value;
+                              });
+                            },
+                          activeColor: Colors.white,
+                          activeTrackColor: Colors.red,
+                        ),
+                      ],
+                    ),
+
+                    SizedBox(
+                      height: 5,
+                    ),
+
+                    DrawerItemWidget(
+                      onTap: () {
+
+                      },
+                      title: "Settings",
+                    ),
+
+                    SizedBox(
+                      height: 5,
+                    ),
+
+                  ],
+                ),
+              ),),
+
           Expanded(
               flex: 3,
-              child: Placeholder()),
+              child: Container(
+                padding: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    
+                    Text("Call For Support",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black54,
+                        fontSize: 20,
+                      ),
+                    ),
+
+                    SizedBox(height: 5,),
+                    
+                    Row(
+                      children: [
+                        
+                        InkWell(
+                            onTap:(){
+                              
+                            },
+                            child: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(16),
+                                color: Colors.red,
+                              ),
+                              child: Icon(Icons.wifi_calling_3_outlined, color: Colors.white,),
+                            ),
+                        ),
+                        
+                        SizedBox(width: 10,),
+                        
+                        Text("162453",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.red,
+                            fontSize: 18,
+                          ),
+                        )
+                      ],
+                    ),
+
+                    Spacer(),
+
+                    InkWell(
+                      onTap: (){
+
+                      },
+                      child: Row(
+                        children: [
+                          Icon(Icons.logout, color: Colors.red,),
+
+                          SizedBox(width: 5,),
+
+                          Text("Log out",
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20,
+                          ),
+                          )
+                        ],
+                      ),
+                    ),
+
+                    SizedBox(height: 30,),
+                    
+                  ],
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+class DrawerItemWidget extends StatelessWidget {
+   DrawerItemWidget({
+    super.key, this.title, this.onTap
+  });
+String? title;
+VoidCallback? onTap;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Row(
+        children: [ 
+          
+          Row(
+            children: [
+              
+            Icon(Icons.arrow_forward_ios),
+            
+            SizedBox(width: 5,),
+            
+            Text("$title",
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              color: Colors.black54,
+              fontSize: 18,
+            ),
+            ),
+            ],
+          ),
+          
+
         ],
       ),
     );
